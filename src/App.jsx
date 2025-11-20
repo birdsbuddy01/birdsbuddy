7eaeef04c09bf973c829e2a9fc7dc5b77b213029
birdsbuddy01 <birdsbuddy01@gmail.com>
2025-11-20 23:29:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -385,6 +385,8 @@ const AboutView = () => {
         <Radio className="w-12 h-12 text-slate-300" />
       </div>
     </div>
+                </div>
+                
                 
                <h1 className="text-3xl font-bold text-white mb-2 font-sans">Birds<span className="font-bold text-slate-500">Buddy</span></h1>
                <p className="text-slate-400 text-sm leading-relaxed">
